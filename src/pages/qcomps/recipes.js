774b46e394930e68cb01a9de44af59e9e31cd3e0
nishyanntomy <nishyanntomy@gmail.com>
2024-02-19 16:10:27 -0500

    add changes for febb 15 activity

--- src/pages/qcomps/recipes.js
+++ src/pages/qcomps/recipes.js
@@ -13,10 +13,20 @@ export const recipes = [{
 }];
 
 export default function RecipeList() {
+  const ingredientsListItems = recipes.ingre
+  const listItems = recipes.map(recipe =>
+    <li key={recipe.id}>
+      <h2>{recipe.name}</h2>
+      <ul>{Array.from(recipe.ingredients).map(ingredient =>
+        <li key={ingredient}>{ingredient}</li>
+      )}</ul>
+    </li>
+  );
+
   return (
     <div>
       <h1>Recipes</h1>
-      {}
+      {listItems}
     </div>
   );
 }
--- src/pages/qcomps/state.js
+++ src/pages/qcomps/state.js
@@ -6,7 +6,8 @@ export default function Gallery() {
   const [showMore, setShowMore] = useState(false);
 
   function handleNextClick() {
-    setIndex(index + 1);
+    setIndex((index + 1)%sculptureList.length);
+    setShowMore(false);
   }
 
   function handleMoreClick() {
